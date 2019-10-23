f6a20f5b09b1712835a3b519831a78ccb463893b
Vincent Liao <vincent.liao@protonmail.com>
2019-10-23 10:27:42 +0700

    created login and register page

--- bookstore-vanilla/app.js
+++ bookstore-vanilla/app.js
@@ -1,34 +1,79 @@
-const registerForm = document.getElementById('register-form')
-registerForm.addEventListener('submit', evt => {
-  const url = 'http://localhost:3000/users/signup/admin'
-  const data = {
-    name: registerForm.name.value,
-    email: registerForm.email.value,
-    password: registerForm.password.value
+class User {
+  constructor(token, name){
+    this.token = token
+    this.name = name
   }
+}
 
-  // This fetch needs cors to be allowed for it to do any put/post op
-  fetch(url, {
-    method: 'PUT',
-    mode: 'cors',
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify(data)
+let user
+
+const registerForm = document.getElementById('register-form')
+if(registerForm){
+  registerForm.addEventListener('submit', evt => {
+    const isAdmin = registerForm.admin.checked
+    let url
+    if(isAdmin){
+      url = 'http://localhost:3000/users/signup/admin'
+    }
+    else{
+      url = 'http://localhost:3000/users/signup/'
+    }
+    const data = {
+      name: registerForm.name.value,
+      email: registerForm.email.value,
+      password: registerForm.password.value
+    }
+  
+    // This fetch needs cors to be allowed for it to do any put/post op
+    fetch(url, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(response => {
+        alert(response.msg)
+        window.location.href = '/'
+      })
+      .catch(err => console.log(err))
+
+    evt.preventDefault()
   })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+}
 
+const loginForm = document.getElementById('login-form')
+if(loginForm){
+  loginForm.addEventListener('submit', evt => {
+    const url = 'http://localhost:3000/users/login'
+    
+    const data = {
+      email: loginForm.email.value,
+      password: loginForm.password.value
+    }
+  
+    // This fetch needs cors to be allowed for it to do any put/post op
+    fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        user = new User(response.data.token, response.data.name)
+        alert('Welcome back ' + user.name)
+        window.location.href = '/'
+      })
+      .catch(err => console.log(err))
 
-  evt.preventDefault()
-})
-
-document.getElementById('get-admin').addEventListener('click', () => {
-  const url = 'http://localhost:3000/users/admin'
-  fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-})
+    evt.preventDefault()
+  })
+}
