5efd1d425e0d2e2bf33d548963bc05726c8a37f6
Vincent Liao <vincent.liao@protonmail.com>
2019-10-26 10:53:03 +0700

    enabled register using json server

--- bookshop-bootstrap/app.js
+++ bookshop-bootstrap/app.js
@@ -14,54 +14,42 @@ if (addBook) {
   }
 }
 
-// handle registration form
-const registerForm = document.getElementById('register-form')
+// handle register form
+const registerForm = document.querySelector('#register-form')
 if (registerForm) {
   registerForm.addEventListener('submit', evt => {
-    const isAdmin = registerForm.admin.checked
-    let url
-    if (isAdmin) {
-      url = 'http://localhost:3000/users/signup/admin'
-    }
-    else {
-      url = 'http://localhost:3000/users/signup/'
-    }
+    evt.preventDefault()
+    const url = 'http://localhost:3000/user'
+
     const data = {
-      name: registerForm.name.value,
-      email: registerForm.email.value,
-      password: registerForm.password.value
+      email: document.querySelector('#register-email').value,
+      password: document.querySelector('#register-password').value,
+      isAdmin: document.querySelector('#admin-checkbox').checked
     }
 
-    // This fetch needs cors to be allowed for it to do any put/post op
     fetch(url, {
-      method: 'PUT',
+      method: 'POST',
       mode: 'cors',
       headers: {
         'Access-Control-Allow-Origin': '*',
         'content-type': 'application/json'
       },
       body: JSON.stringify(data)
+    }).then(response => {
+      window.location.href = '/'
     })
-      .then(response => response.json())
-      .then(response => {
-        alert(response.msg)
-        window.location.href = '/'
-      })
-      .catch(err => console.log(err))
-
-    evt.preventDefault()
   })
 }
 
 // handle login form
-const loginForm = document.getElementById('login-form')
+const loginForm = document.querySelector('#login-form')
 if (loginForm) {
   loginForm.addEventListener('submit', evt => {
-    const url = 'http://localhost:3000/users/login'
+    const url = 'http://localhost:3000/user'
 
     const data = {
-      email: loginForm.email.value,
-      password: loginForm.password.value
+      email: document.querySelector('#login-email').value,
+      password: document.querySelector('#login-password').value
     }
 
     // This fetch needs cors to be allowed for it to do any put/post op
@@ -75,21 +63,21 @@ if (loginForm) {
       body: JSON.stringify(data)
     })
       .then(response => {
-        console.log(response)
         if (response.ok) {
           return response.json()
         } else {
-          alert('Wrong email or password')
+          alert('Something went wrong')
           throw new Error(String(response.status) + ' ' + response.statusText)
         }
       })
       .then(response => {
-        console.log(typeof response.data.isAdmin)
-        localStorage.setItem('name', response.data.name)
-        localStorage.setItem('token', response.data.token)
-        localStorage.setItem('isAdmin', response.data.isAdmin)
-        alert('Welcome back ' + response.data.name)
-        window.location.href = '/'
+        // localStorage.setItem('name', response.data.name)
+        // localStorage.setItem('token', response.data.token)
+        // localStorage.setItem('isAdmin', response.data.isAdmin)
+        // alert('Welcome back ' + response.data.name)
+        console.log(response)
+        alert('welcome!')
+        alert('successfully login')
       })
       .catch(err => console.log(err))
 
@@ -135,12 +123,11 @@ if (addBookForm) {
   })
 }
 
-// insert book to table
-const bookTable = document.getElementById('book-table')
+// get books and render it on table
+const bookTable = document.querySelector('#book-table')
 if (bookTable) {
-  const url = 'http://localhost:3000'
-  const buyUrl = 'http://localhost:3000/buy'
-  const isAdmin = localStorage.getItem('isAdmin')
+  const url = 'http://localhost:3000/books'
+  const tbody = bookTable.children[1]
 
   fetch(url)
     .then(response => response.json())
@@ -151,51 +138,16 @@ if (bookTable) {
           <td>${book.title}</td>
           <td>${book.author}</td>
           <td>${book.isbn}</td>
+          <td>
+            <button type="button" class="btn btn-sm btn-secondary main-color-bg" data-toggle="modal"
+              data-target="#buyModal">
+              Buy
+            </button>
+          </td>
         `
-        // remember, local storage can only store string
-        // if customer, then add the buy button
-        if(isAdmin == 'false'){
-          tableHtml += `<td><button id="buy-button">Buy</button></td>`
-        }
-
         row.innerHTML = tableHtml
-
-        bookTable.appendChild(row)
+        tbody.appendChild(row)
       });
     })
     .catch(err => console.log(err))
-
-  bookTable.addEventListener('click', evt => {
-    if (evt.target.id == 'buy-button') {
-      const btn = evt.target
-      const token = localStorage.getItem('token')
-
-      // first parent element is the td of buton, second is the tr
-      const row = btn.parentElement.parentElement
-
-      // [2] is because isbn is the third column
-      const isbn = row.children[2].innerHTML
-      const data = { isbn: isbn }
-
-      fetch(buyUrl, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'content-type': 'application/json',
-          'Authorization': 'Bearer: ' + token
-        },
-        body: JSON.stringify(data)
-      })
-        .then(response => {
-          if (response.status == 403) {
-            alert('You are not authorized!')
-          }
-          else if (response.ok) {
-            alert('Order successful!')
-          }
-        })
-        .catch(err => console.log(err))
-    }
-  })
 }
