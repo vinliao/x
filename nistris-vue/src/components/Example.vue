fd427ce1caa775f44bfaae85adaebe6ee5c58bfd
Vincent Liao <vincent.liao@protonmail.com>
2019-11-14 12:14:01 +0700

    initial commit on nistris-vue

---
+++ nistris-vue/src/components/Example.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <p>hello world</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+
+.container {
+  @include mixin-example;
+}
+
+</style>
