4ddf615dbf27fc2ea98f1b5211bdbee81824116e
Vincent Liao <vincent.liao@protonmail.com>
2019-11-15 18:43:55 +0700

    added scripts folder

---
+++ scripts/move-to-dropbox.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# TODO: multiple .epub file breaks the script
+
+EPUB=$(ls ~/Downloads | grep '.epub')
+
+if [ "$EPUB" = "" ]; then
+    echo There is no epub to move!
+else
+    # Using quotation mark will treat the variable as a whole
+    # instead of splitting it into an "array of texts"
+    echo "$EPUB"
+    mv ~/Downloads/"$EPUB" ~/Dropbox/epubs
+fi
