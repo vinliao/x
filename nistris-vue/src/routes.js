5e6373d926ac6a4237b525c1b77c26cde1ae5569
Vincent Liao <vincent.liao@protonmail.com>
2019-12-12 13:00:43 +0700

    create fab with modal

--- nistris-vue/src/routes.js
+++ nistris-vue/src/routes.js
@@ -1,9 +1,11 @@
 import LandingPage from '@/pages/LandingPage'
 import AppHomePage from '@/pages/AppHomePage'
 import AppChooseUploadPage from '@/pages/AppChooseUploadPage'
+import TestComponent from '@/components/TestComponent'
 
 export const routes = [
   { path: '/', component: LandingPage, name: 'landingPage' },
   { path: '/app', component: AppHomePage, name: 'appHomePage' },
   { path: '/app/upload', component: AppChooseUploadPage, name: 'appChooseUploadPage' },
+  { path: '/test', component: TestComponent, name: 'testComponent' },
 ]
