6cbed5df087bc544697b26cca785ee8b4d579ecf
Vincent Liao <vincent.liao@protonmail.com>
2020-01-08 19:55:23 +0700

    created pvpn-connect.sh

--- scripts/pvpn-connect.sh
+++ scripts/pvpn-connect.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# ping google
-# if fail, reconnect
-# if can't reconnect, then the internet is dead
-
 while true;
 do
-  ping -c1 google.com
+  ping -c 1 -W 3 -q google.com
   # if ping isn't successful
   if [ $? -ne 0 ]
   then
 
     protonvpn c -r
-
+    # if first connect isn't succesful
+    # then connect again
     while [ $? -ne 0 ];
     do
         protonvpn c -r
