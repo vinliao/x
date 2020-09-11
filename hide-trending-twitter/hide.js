8a2441f7f592b5df611a89ecd863787876878f05
Vincent Liao <vincent.liao@protonmail.com>
2020-09-11 21:19:56 +0700

    fix bug #14

--- hide-trending-twitter/hide.js
+++ hide-trending-twitter/hide.js
@@ -1,10 +1,11 @@
 const hide = async () => {
-  setTimeout(() => {
-    const followDiv = document.querySelector("[aria-label='Who to follow']").parentElement
-    followDiv.style.display = "none"
-
-    const trendingDiv = document.querySelector("[aria-label='Timeline: Trending now']").parentElement
-    trendingDiv.style.display = "none"
+  setInterval(() => {
+    if (document.querySelector("[aria-label='Who to follow']").parentElement != null) {
+      document.querySelector("[aria-label='Who to follow']").parentElement.style.display = "none"
+    }
+    if (document.querySelector("[aria-label='Timeline: Trending now']").parentElement != null) {
+      document.querySelector("[aria-label='Timeline: Trending now']").parentElement.style.display = "none"
+    }
   }, 2000);
 }
 
