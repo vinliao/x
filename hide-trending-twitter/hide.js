0e12d41e43f4bb9d08b77696e3808a526ce15867
Vincent Liao <vincent.liao@protonmail.com>
2020-09-13 13:35:49 +0700

    fix bug #14 (again)

--- hide-trending-twitter/hide.js
+++ hide-trending-twitter/hide.js
@@ -1,10 +1,13 @@
-const hide = async () => {
+const hide = () => {
   setInterval(() => {
-    if (document.querySelector("[aria-label='Who to follow']").parentElement != null) {
-      document.querySelector("[aria-label='Who to follow']").parentElement.style.display = "none"
-    }
-    if (document.querySelector("[aria-label='Timeline: Trending now']").parentElement != null) {
+
+    // TODO: for some reason, if the condition is false here, it won't go on the code below it.
+    if (document.querySelector("[aria-label='Timeline: Trending now']").parentElement !== null) {
       document.querySelector("[aria-label='Timeline: Trending now']").parentElement.style.display = "none"
+    }
+
+    if (document.querySelector("[aria-label='Who to follow']").parentElement !== null) {
+      document.querySelector("[aria-label='Who to follow']").parentElement.style.display = "none"
     }
   }, 2000);
 }
