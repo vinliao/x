5f050ed759ed3d72bdc31a8c095cda031ee7c592
Vincent Liao <vincent.liao@protonmail.com>
2020-10-16 20:52:50 +0700

    wrote my version of sending tweets

--- tweet-chopper/index.js
+++ tweet-chopper/index.js
@@ -11,18 +11,16 @@ const client = new Twitter({
   access_token_secret: credentials.access_token_secret,
 })
 
-// this is a copy pasted code, maybe read it in the future
-// ... maybe, or maybe not. LOL!
-async function tweetThread(thread) {
-  let lastTweetID = "";
-  for (const status of thread) {
-    const tweet = await client.post("statuses/update", {
-      status: status,
-      in_reply_to_status_id: lastTweetID,
+const sendTweets = async () => {
+  let lastTweetId = '';
+  for (let i = 0; i < tweets.length; i++) {
+    const tweet = await client.post('statuses/update', {
+      status: tweets[i],
+      in_reply_to_status_id: lastTweetId,
       auto_populate_reply_metadata: true
-    });
-    lastTweetID = tweet.id_str;
+    })
+    lastTweetId = tweet.id_str;
   }
 }
 
-tweetThread(tweets).catch(console.error);
+sendTweets().catch((err) => console.error(err));
