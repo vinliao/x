04632b4c40a7cf457f12f36a4010e9479de17183
Vincent Liao <vincent.liao@protonmail.com>
2020-11-25 10:36:51 +0700

    implement read from stdin

--- c/text-wrapper.c
+++ c/text-wrapper.c
@@ -6,44 +6,55 @@
 
 // and create a make file to "install" it in /usr/local/bin
 
-// input: file
+// input: stdin of raw text
 // output: text
 // end result: progaramName fileName spits the formatted output 
-// Edit: I want to be able to wl-paste | ./program.out
 
 int main(int argc, char *argv[]){
-    if(argc != 2){
-        printf("Exactly one argument needed, which is the filename\n");
-        exit(1);
-    }
-
-    // data type file; pointer named fp
-    FILE *fp;
-    char ch;
     int counter = 0;
     int deviation = 10;
     int maxLength = 75;
+    char ch;
 
-    // argv[1] is filename from argument
-    fp = fopen(argv[1], "r");
+    if(argc > 1){
+        // text input
+        FILE *fp;
 
-    if(fp == NULL){
-        printf("File not found.\n");
-        exit(1);
-    }
+        // argv[1] is filename from argument
+        fp = fopen(argv[1], "r");
 
-    while((ch = fgetc(fp)) != EOF){
-        printf("%c", ch);
-        counter++;
-
-        if(counter >= maxLength - deviation && ch == ' '){
-            printf("\n");
-            counter = 0;
-        } else if (ch == '\n') {
-            counter = 0;
+        if(fp == NULL){
+            printf("File not found.\n");
+            exit(1);
         }
-    }
 
-    fclose(fp);
-    return 0;
+        while((ch = fgetc(fp)) != EOF){
+            printf("%c", ch);
+            counter++;
+
+            if(counter >= maxLength - deviation && ch == ' '){
+                printf("\n");
+                counter = 0;
+            } else if (ch == '\n') {
+                counter = 0;
+            }
+        }
+
+        fclose(fp);
+        return 0;
+    } else {
+        // read from stdin
+        while((ch = getchar()) != EOF){
+            printf("%c", ch);
+            counter++;
+
+            if(counter >= maxLength - deviation && ch == ' '){
+                printf("\n");
+                counter = 0;
+            } else if (ch == '\n') {
+                counter = 0;
+            }
+        }
+
+    }
 }
